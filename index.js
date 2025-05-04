2817ab6efcd91c63bdbad92949f3d976afa0ff36
AylexCODE <AlexanderlMC@gmail.com>
2025-05-04 14:26:01 +0800

    Finished Realtime Requirements

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+
+const server = require('http').createServer(express);
+
+const socketio = require('socket.io')(server, {
+    cors: { origin: '*' }
+});
+
+socketio.on('connection', socket => {
+    //socket.emit('message', 'Hello World!');
+    console.log("A user connected successfully!");
+
+    socket.on('update_admin', socket => {
+        socketio.emit('update_admin', 'OK');
+    });
+
+    socket.on('update_user', socket => {
+        socketio.emit('update_user', 'OK');
+    });
+
+    socket.on('message', socket => {
+        socketio.emit('message', socket);
+    });
+});
+
+server.listen(3000, () => 
+    console.log("Socket is listening...")
+);
