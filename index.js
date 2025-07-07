ad6419fc4f8c1826ddc8efffe2cd38b96e388c97
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-07-07 15:52:29 +0800

    added callback to 'connectToConnection event'

--- index.js
+++ index.js
@@ -18,10 +18,19 @@ socketIo.on('connection', socket => {
         console.log("Joined connection", id);
     });
 
-    socket.on('connectToConnection', (id) => {
-        socket.join(connections[id]);
-        
-        console.log("Joined connection", id);
+    socket.on('connectToConnection', (id, callback) => {
+        if(connections[id]){
+            socket.join(connections[id]);
+            callback({
+                status: "connected"
+            });
+            console.log("Joined connection", id);
+        }else{
+            callback({
+                status: "thatIdDoesNotExist"
+            });
+            console.log("Connection with ID: ", id, "does not exist");
+        }
     });
 
     socket.on('sendMessage', (data) => {
